463adfb01b5e5ce95b41171f45b01bd8e49b29ed
purocean <purocean@gmail.com>
2024-12-30 23:30:20 +0800

    fix: add undo stop before executing spellchecker edits

--- src/spellchecker.ts
+++ src/spellchecker.ts
@@ -194,6 +194,7 @@ export function getSpellchecker(
 
             const { range, suggestion } = args
 
+            editor.pushUndoStop()
             editor.executeEdits('spellchecker', [{
                 range,
                 text: suggestion,
